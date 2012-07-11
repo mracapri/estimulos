746df94bea4413a0fa9a5c1a74e014dc035dbac7
David <dp_play.music@hotmail.com>
2012-07-11 10:14:18 -0500

    Agregar la ventana inicio sesion

---
+++ InicioSesion.php
@@ -0,0 +1,84 @@
+<html>
+<head>
+
+	<!-- Meta -->
+	<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+	<meta name="author" content="Noemi Mejia, David Perez, Mario Rivera" />
+	<meta name="keywords" content="programa de estimulos utvm" />
+	<meta name="description" content="Programa de estimulos para Universidad Tecnologica del Valle del Mezquital" />
+	<meta name="robots" content="all" />	
+	
+	<!-- Titulo del documento -->		
+	<title>Programa de Estimulos</title>
+
+	<!-- css bootstrap framework -->
+	<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+	<link rel="stylesheet" href="css/bootstrap-responsive.min.css" type="text/css">
+
+	<!-- css application -->
+	<link rel="stylesheet" href="css/main.css" type="text/css">
+
+   
+   	<!-- js bootstrap framework -->
+	<script type='text/javascript' src='js/jquery-1.7.2.min.js'></script> <!-- esta libreria siempre tiene que ir al inicio -->
+	<script type='text/javascript' src='js/bootstrap.js'></script>
+	<script type='text/javascript' src='js/bootstrap-modal.js'></script>
+	<script type='text/javascript' src='js/bootstrap-dropdown.js'></script>
+	<script type='text/javascript' src='js/jquery-ui-1.8.21.custom.min.js'></script>
+	<script type='text/javascript' src='js/jquery-ui-1.8.21.custom.min.js'></script>
+	
+
+
+	<!-- js application -->
+	<script type='text/javascript' src='js/main.js'></script>
+
+	<!-- YUI -->
+	<script src="http://yui.yahooapis.com/3.5.1/build/yui/yui-min.js"></script>
+
+</head>
+
+<body>
+
+	<!-- barra negro -->
+	<div class="navbar">
+	    <div class="navbar-inner">
+		    <div class="container">
+				<a class="brand" href="#">Programa de Estimulos</a>
+				<ul class="nav">
+				    <li>
+				    	<a href="#">PREDA</a>
+				    </li>
+
+					<li class="active">
+						<a id="nombre-persona" href="#">BIENVENIDO</a>
+					</li>
+					<li class="brand">
+					<a href="#">INICIA SESION</a>
+					</li>
+				</ul>
+		    </div>
+	    </div>
+	</div>
+
+	<!-- contenedor principal -->
+	<div class="container">
+		<div class="row-fluid">
+			<div class="span4 seccion5">
+				<div class="seccion1-1">
+				</div>
+				<form class="well">
+					<div class="span2">
+						INICIAR SESION
+					</div>
+					<label>Usuario</label>
+					<input class="span2" type="text" placeholder="Nombre del Usuario" name="usuario"/>
+					<label>Constraseña</label>
+					<input class="span2" type="text" placeholder="Contraseña" name="contrasena"/>
+				</form>
+			</div>
+
+		</div>
+	</div>
+
+</body>
+</html>
