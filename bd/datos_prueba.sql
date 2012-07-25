b273c776dd11e7c421083a85e2391c03d05ee897
Mario Rivera Angeles <mra_capri@msn.com>
2012-07-25 13:50:30 -0500

    Homogenizando ambientes de prueba

--- bd/datos_prueba.sql
+++ bd/datos_prueba.sql
@@ -1,11 +1,22 @@
-insert into evaluador (RFC_evaluador, nombre, tipo, anio) values ('RIAM821203', 'Mario Rivera Angeles', 'I', 2012);
-insert into evaluador (RFC_evaluador, nombre, tipo, anio) values ('TAAM801205', 'Jose Juan Martinez Perez ', 'I', 2012);
 
+/* Evalacion actual */
 insert into evaluacion (anio, descripcion, fecha_captura, fecha_limite_captura, fecha_evaluacion, fecha_limite_evaluacion) 
-values (2011, 'Evaluacion 2012', DATE('2012/01/12'), DATE('2012/01/12'), DATE('2012/01/12'), DATE('2012/01/12'));
+values (2012, 'Evaluacion 2012', DATE('2012/07/25'), DATE('2012/07/29'), DATE('2012/08/01'), DATE('2012/08/05'));
 
-insert into evaluacion (anio, descripcion, fecha_captura, fecha_limite_captura, fecha_evaluacion, fecha_limite_evaluacion) 
-values (2010, 'Evaluacion 2012', DATE('2012/01/12'), DATE('2012/01/12'), DATE('2012/01/12'), DATE('2012/01/12'));
+/* agregando evaluadores */
 
+insert into evaluador (RFC_evaluador, nombre, tipo, anio) values ('TETE750801', 'Elia Trejo Trejo', 'I', 2012);
+insert into evaluador (RFC_evaluador, nombre, tipo, anio) values ('MENN901110', 'Noemi Mejia Nieto ', 'E', 2012);
+
+/* Reportes del sistema */
 insert into reportes (id_reporte, nombre, path) values (1, 'Reporte 1', 'reporte1.php');
 insert into reportes (id_reporte, nombre, path) values (2, 'Reporte 2', 'reporte2.php');
+
+/* Agregando participantes */
+insert into participantes (rfc, estado, anio) values ('SAZL700719', '0', 2012);
+insert into participantes (rfc, estado, anio) values ('BELJ741120', '0', 2012);
+insert into participantes (rfc, estado, anio) values ('ROMJ750603', '0', 2012);
+insert into participantes (rfc, estado, anio) values ('TEDO651008', '0', 2012);
+
+
+
