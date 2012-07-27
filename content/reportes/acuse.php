ce73503ac653608c0a2c690b327a11fed41c00a1
David <dp_play.music@hotmail.com>
2012-07-27 14:49:29 -0500

    Reportes de pdf

---
+++ content/reportes/acuse.php
@@ -0,0 +1,127 @@
+<?php
+session_start();
+require('../../ext-lib/fpdfp/fpdf.php');
+include "../../lib/mysql.php";
+
+$rfcDocente = $_SESSION['rfcDocente'];
+
+reporteRegulares($rfcDocente);
+function reporteRegulares($rfcDocente)
+{
+	$conexion = getConnection();
+	class PDF extends FPDF
+	{
+		//Cabecera de página
+		function Header()
+			{
+				//Logo
+				$this->Image('../../img/logo.jpg',10,8,33);
+				//Arial bold 15
+				$this->SetFont('Arial','B',15);
+				//Movernos a la derecha
+				$this->Cell(85);
+				//Título
+				$this->Cell(35,10,'Universidad Tecnológica del Valle del Mezquital.-'.$rfcDocente,0,0,'C');
+				$this->Ln(8);
+				$this->SetFont('Arial','B',12);
+				$this->Cell(80);
+				$this->Cell(35,10,'Acuse de envio.',0,0,'C');
+				$this->Ln(15);
+				//Fecha de impresion del reporte
+				$this->SetX(10);
+				$this->SetFont('Arial','',9);
+				$mesnum = date('m') + 0;
+				$mes = array('','Enero','Febrero','Marzo','Aril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre');
+				$this->Cell(32,3,'Fecha de Impresión:',0,0,'LR',0,'L');
+				
+				mysql_query("SET NAMES UTF8");
+				$datos = mysql_fetch_array(mysql_query("SELECT `folio`,`nombre`,`RFC`,`idempleado`,`programa_educativo` FROM `acuse` WHERE `idempleado` LIKE 'E999'"));
+				
+
+				$this->Cell(40,3,utf8_decode(date('d')." de ".$mes[$mesnum]." del ".date('Y')),0,0,'L');
+				$this->Ln(4);
+				$this->Cell(32,3,'Folio:',0,0,'l');
+				$this->Cell(32,3,utf8_decode($datos[0]),0,0,'l');
+				$this->Ln(4);
+				$this->Cell(32,3,'Docente:',0,0,'l');
+				$this->Cell(120,3,utf8_decode($datos[1]),0,0,'l');
+				$this->Ln(4);
+				$this->Cell(32,3,'RFC:',0,0,'l');
+				$this->Cell(32,3,utf8_decode($datos[2]),0,0,'l');
+				$this->Ln(4);
+				$this->Cell(32,3,'No. Empleado:',0,0,'l');
+				$this->Cell(32,3,utf8_decode($datos[3]),0,0,'l');
+				$this->Ln(4);
+				$this->Cell(32,3,'Programa E.:',0,0,'l');
+				$this->Cell(150,3,utf8_decode($datos[4]),0,0,'l');
+				
+				//Salto de línea
+				$this->Ln(8);
+				
+			}
+		function tabla()
+			{
+				$this->SetX(5);
+				//Arial 12
+				$this->SetFont('Helvetica','B',12);
+				//Color de fondo
+				$this->SetFillColor(200,220,220);
+				//Título
+				$this->Cell(205,5,'A C U S E',0,0,'C',true);
+				//Salto de línea
+				$this->Ln(10);
+				
+			}
+		function body($rfcDocente)
+			{
+			mysql_query("SET NAMES UTF8");
+			$datos = mysql_fetch_array(mysql_query("SELECT `nombre`FROM `acuse` WHERE `idempleado` LIKE 'E999'"));
+				
+				
+					$this->SetX(10);
+					$this->SetFont('Arial','',10);
+					$this->MultiCell(195,5,'Este método permite imprimir texto con saltos de línea. Estos pueden ser automáticos (tan pronto como el texto alcanza 
+el borde derecho de la celda) o explícito (via el carácter \n). Tantas celdas como sean necesarias son creadas,
+uno debajo de otra. 
+El texto puede ser alineado, centrado o justificado. El bloque de celda puede ser enmarcado y el fondo impreso.',0,'J',false);
+											
+					$this->Ln(140);
+					
+					$this->Cell(205,5,'______________________',0,0,'C',false);
+					$this->SetFont('Arial','B',10);
+					$this->Ln(5);
+					$this->Cell(205,5,'Firma del docente',0,0,'C',false);
+					$this->Ln(5);
+					$this->SetFont('Arial','',10);
+					$this->Cell(205,5,utf8_decode($datos[0]),0,0,'C',false);
+					$this->Ln(5);
+												
+			}
+			
+
+		//Pie de página
+		function Footer()
+			{
+				//Leyenda de derechos reservados
+				$this->SetY(-13);
+				$this->SetFont('Arial','',8);
+				$this->Cell(205,10,'UTVM Derechos Reservados.',0,0,'C');
+				//Posición: a 1,5 cm del final
+				$this->SetY(-10);
+				//Arial italic 8
+				$this->SetFont('Arial','I',8);
+				//Número de página
+				$this->Cell(205,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+			}
+	}
+}
+
+//Creación del objeto de la clase heredada
+$pdf=new PDF('P','mm','letter');
+$pdf->AliasNbPages();
+$pdf->AddPage();
+$pdf->SetFont('Times','',12);
+$pdf->tabla();
+$pdf->body($rfcDocente);
+$pdf->Output();
+?>
--- content/reportes/reporteestado.php
+++ content/reportes/reporteestado.php
@@ -17,7 +17,7 @@ function reporteRegulares($rfcDocente)
 				//Arial bold 15
 				$this->SetFont('Arial','B',15);
 				//Movernos a la derecha
-				$this->Cell(80);
+				$this->Cell(85);
 				//Título
 				$this->Cell(35,10,'Universidad Tecnológica del Valle del Mezquital.',0,0,'C');
 				$this->Ln(8);
@@ -26,7 +26,8 @@ function reporteRegulares($rfcDocente)
 				$this->Cell(35,10,'Reporte de estado de captura del docente.',0,0,'C');
 				$this->Ln(15);
 				//Fecha de impresion del reporte
-				$this->SetFont('Arial','',10);
+				$this->SetX(5);
+				$this->SetFont('Arial','',9);
 				$mesnum = date('m') + 0;
 				$mes = array('','Enero','Febrero','Marzo','Aril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre');
 				$this->Cell(35,3,'Fecha de Impresión:',0,0,'LR',0,'L');
@@ -38,34 +39,42 @@ function reporteRegulares($rfcDocente)
 			}
 		function tabla()
 			{
-				$this->SetX(10);
+				$this->SetX(5);
 				//Arial 12
-				$this->SetFont('Helvetica','',10);
+				$this->SetFont('Helvetica','',9);
 				//Color de fondo
-				$this->SetFillColor(200,220,255);
+				$this->SetFillColor(200,220,220);
 				//Título
-				$this->Cell(40,5,'Categoria',0,0,'C',true);
-				$this->Cell(40,5,'Porcentaje categoria',0,0,'C',true);
-				$this->Cell(40,5,'Indicador',0,0,'C',true);
-				$this->Cell(40,5,'Porcentaje indicador',0,0,'C',true);
-				$this->Cell(30,5,'Estado',0,0,'C',true);
+				$this->Cell(50,5,'Categoria',1,0,'C',true);
+				$this->Cell(20,5,'Porcentaje',1,0,'C',true);
+				$this->Cell(90,5,'Indicador',1,0,'C',true);
+				$this->Cell(20,5,'Porcentaje',1,0,'C',true);
+				$this->Cell(25,5,'Estado',1,0,'C',true);
 				//Salto de línea
 				$this->Ln(10);
 				
 			}
 		function body($rfcDocente)
 			{
-				$idCategoria = 1;
+				$idCategoria = 7;
 				
 				
 				//Consulta
 				mysql_query("SET NAMES UTF8");
-					$indicador = mysql_query("SELECT c.id_categoria as id_categoria, c.descripcion  as descripcion_categoria, i.id_indicador as id_indicador, i.descripcion as descripcion_indicador,ci.id_categoriaindicador as categoria_indicador,(select max(porcentaje) from porcentaje_indicador where id_categoriaindicador = ci.id_categoriaindicador) as porcentaje, (select COALESCE(max(id_categoriaindicador),0) from asignacion_indicador where id_categoriaindicador = ci.id_categoriaindicador) as estatus FROM 	categoria As c, indicador As i, categoria_indicador As ci WHERE ci.id_categoria = ".$idCategoria." and c.id_categoria = ci.id_categoria and i.id_indicador = ci.id_indicador") or die('error');
+					$indicador = mysql_query("SELECT c.id_categoria as id_categoria, c.descripcion  as descripcion_categoria,i.descripcion as descripcion_indicador,(select max(porcentaje) from porcentaje_indicador where id_categoriaindicador = ci.id_categoriaindicador) as porcentaje, (select COALESCE(max(id_categoriaindicador),0) from asignacion_indicador where id_categoriaindicador = ci.id_categoriaindicador) as estatus FROM 	categoria As c, indicador As i, categoria_indicador As ci WHERE ci.id_categoria = ".$idCategoria." and c.id_categoria = ci.id_categoria and i.id_indicador = ci.id_indicador") or die('error');
 				
 				while($indica = mysql_fetch_array($indicador)){
-					$this->SetX(10);
-					$this->Cell(40,5,utf8_decode($indica[1]),'C',true);
-					$this->Cell(40,5,'none','C',true);
+					$this->SetLineWidth(.2);
+					$this->SetX(5);
+					
+					$this->SetFont('Helvetica','',8);
+					$this->Cell(50,5,utf8_decode($indica[1]),1,0,'l');
+					$this->Cell(20,5,'No calculado',1,0,'C');
+					$this->Cell(90,5,utf8_decode($indica[2]),1,'T');
+					$this->Cell(20,5,utf8_decode($indica[3]),1,0,'C');
+					$this->Cell(25,5,'Capturado',1,0,'C');
+					$this->Ln(5);
+					
 				}
 								
 			}
@@ -75,11 +84,11 @@ function reporteRegulares($rfcDocente)
 		function Footer()
 			{
 				//Leyenda de derechos reservados
-				$this->SetY(-18);
+				$this->SetY(-13);
 				$this->SetFont('Arial','',8);
 				$this->Cell(0,10,'UTVM Derechos Reservados.',0,0,'C');
 				//Posición: a 1,5 cm del final
-				$this->SetY(-15);
+				$this->SetY(-10);
 				//Arial italic 8
 				$this->SetFont('Arial','I',8);
 				//Número de página
@@ -89,7 +98,7 @@ function reporteRegulares($rfcDocente)
 }
 
 //Creación del objeto de la clase heredada
-$pdf=new PDF();
+$pdf=new PDF('P','mm','letter');
 $pdf->AliasNbPages();
 $pdf->AddPage();
 $pdf->SetFont('Times','',12);
@@ -97,4 +106,3 @@ $pdf->tabla();
 $pdf->body($rfcDocente);
 $pdf->Output();
 ?>
-
