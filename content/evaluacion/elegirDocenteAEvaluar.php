d356d7763a044357c327e50df077ef0ab9a4347d
Mario Rivera Angeles <mra_capri@msn.com>
2012-07-30 11:03:59 -0500

    Cambios en evaluacion

--- content/evaluacion/elegirDocenteAEvaluar.php
+++ content/evaluacion/elegirDocenteAEvaluar.php
@@ -55,7 +55,7 @@
 					</li>
 					<a class="brand-2" href="#">Elegir docente a evaluar</a>
 					<div class="span2 position">
-					<div class="btn-group pull-right open">
+					<div class="btn-group pull-right">
 						<a class="btn dropdown-toggle" href="#" data-toggle="dropdown">
 							<i class="icon-user"></i>
 								<?php echo $_SESSION['rfcEvaluador']; ?>
