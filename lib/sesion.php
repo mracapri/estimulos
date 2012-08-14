01385a0c3d647df247c5bc62153aad7c5e298aee
Mario Rivera Angeles <mra_capri@msn.com>
2012-08-14 00:24:20 -0500

    Revision de la session

--- lib/sesion.php
+++ lib/sesion.php
@@ -1,40 +1,31 @@
 <?php 
 	/*
-		variable para prender la seguridad del sistema
-
-		1 - ACTIVADA
-		0 - DESACTIVADA
-
-		Produccion: siempre en ACTIVADA
+		verifica si la seccion aun es activa y lo notifica al usuario
 	*/
-	define("SEGURIDAD", "0"); 
 
 	function verificarSesionDelUsuario(){
-		if(SEGURIDAD == 1){
-			$alertaHtml = "";
-			$alertaHtml .= "<span id=\"alerta-sesion\" style=\"margin-left:20px;\" class=\"label label-important\">";
-			$alertaHtml .= 		"<a href=\"content/login/login.php\" style=\"color:white; \">";
-			$alertaHtml .= 			"Vuelva a iniciar sesion";
-			$alertaHtml .= 		"</a>";
-			$alertaHtml .= "</span>";
+		$alertaHtml = "";
+		$alertaHtml .= "<span id=\"alerta-sesion\" style=\"margin-left:20px;\" class=\"label label-important\">";
+		$alertaHtml .= 		"<a href=\"/estimulos/\" style=\"color:white; \">";
+		$alertaHtml .= 			"Vuelva a iniciar sesion";
+		$alertaHtml .= 		"</a>";
+		$alertaHtml .= "</span>";
 
-			/* definiendo script para agregar alerta */
-			$scriptJQuery = "";
-			$scriptJQuery .= "<script language='javascript'>";
-			$scriptJQuery .= 	"$(document).ready(function(){";
-			$scriptJQuery .= 		"if($('#alerta-sesion').size() == 0){";
-			$scriptJQuery .= 			"$('".$alertaHtml."').appendTo(\"#nombre-persona\");";
-			$scriptJQuery .= 		"}";
-			$scriptJQuery .= 	"});";
-			$scriptJQuery .= "</script>";
+		/* definiendo script para agregar alerta */
+		$scriptJQuery = "";
+		$scriptJQuery .= "<script language='javascript'>";
+		$scriptJQuery .= 	"$(document).ready(function(){";
+		$scriptJQuery .= 		"if($('#alerta-sesion').size() == 0){";
+		$scriptJQuery .= 			"$('".$alertaHtml."').appendTo(\"#nombre-persona\");";
+		$scriptJQuery .= 		"}";
+		$scriptJQuery .= 	"});";
+		$scriptJQuery .= "</script>";
 
-			/* comprobando la seguridad de la app */
-			if(!isset($_SESSION['usuarioFirmado'])){
-				echo $scriptJQuery;
-				return false;
-			}else{
-				return true;
-			}			
+		/* comprobando la seguridad de la app */
+		if(!isset($_SESSION['usuarioFirmado'])){
+			echo $scriptJQuery;
+			exit;
+			return false;
 		}else{
 			return true;
 		}
